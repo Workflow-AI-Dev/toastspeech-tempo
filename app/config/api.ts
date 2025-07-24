17274fbd6fd1bb264f2748281f32f8d755305e73
Bashini Weerasinghe <weerasinghebashini@gmail.com>
2025-07-24 15:08:19 +0530

    ngrok 2

--- app/config/api.ts
+++ app/config/api.ts
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://10.7.2.242";
-export const BASE_URL = "https://ea6f256b748e.ngrok-free.app";
+export const BASE_URL = "https://69b197f1092e.ngrok-free.app";
 export const GOOGLE_CLIENT_ID =
   "454279169261-8sljbk4qjgfnrms4hddr4d2chc1ghbon.apps.googleusercontent.com";
