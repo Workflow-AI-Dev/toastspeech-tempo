009a248b6babe4d5040689b7bdb445d4abafe662
Bashini Weerasinghe <weerasinghebashini@gmail.com>
2025-10-23 12:32:42 +0530

    import error fixed

--- app/styles/feedback-styles.tsx
+++ app/styles/feedback-styles.tsx
@@ -1,7 +1,4 @@
 import { StyleSheet } from "react-native";
-import { Dimensions } from "react-native";
-
-const { height } = Dimensions.get("window");
 
 export const styles = StyleSheet.create({
   loaderContainer: {
--- app/styles/realistic-progress-loader-styles.tsx
+++ app/styles/realistic-progress-loader-styles.tsx
@@ -1,4 +1,7 @@
 import { StyleSheet } from "react-native";
+import { Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window");
 
 export const styles = StyleSheet.create({
   container: {
